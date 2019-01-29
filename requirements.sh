f20eb235e45c63f3487441372049754ac7d56389
hamzamohdzubair <hamzamohdzubair@gmail.com>
2019-01-29 16:55:38 +0530

    Update requirements.sh from editor

--- requirements.sh
+++ requirements.sh
@@ -10,7 +10,9 @@ pip3 install pandas==0.23.3
 # pip3 install numpy==1.15.0
 # pip3 install seaborn==0.9.0
 # pip3 install bokeh==0.13.0
+
 pip3 install nltk==3.4
+python -m nltk.downloader all
 
 # Include datasets in the image
 
